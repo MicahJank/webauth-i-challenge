a49731aff1108709f05fc848ecdb0fff79882a64
MicahJank <MicahJank@users.noreply.github.com>
2019-12-04 21:07:25 -0600

    added to the users router imported the users model and set up a get call to the api/users endpoint

---
+++ users/users-router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const Users = require('./users-model.js');
+
+//authrequire here
+
+
+router.get('/', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+module.exports = router;
