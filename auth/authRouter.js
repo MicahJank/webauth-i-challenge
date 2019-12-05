633bda25f469f8a945afd3ce7c56ece0891e8e09
MicahJank <MicahJank@users.noreply.github.com>
2019-12-04 21:01:06 -0600

    added to the authRouter file create a post to the login endpoint that should return a welcome message for the user if it can find the user in the database

---
+++ auth/authRouter.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+//bcrypt here
+
+//credential middleware here
+
+const Users = require('../users/users-model.js');
+
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+        if(user) {
+            res.json({ message: `Welcome ${user.username}` });
+        } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+        };
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Couldnt log in.',
+    error: err })
+    })
+});
+
+module.exports = router;
