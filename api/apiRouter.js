290f09279c49836e68bbf2c96c4274bf7aad0a87
MicahJank <MicahJank@users.noreply.github.com>
2019-12-08 15:07:34 -0600

    added logoutRouter to the apiRouter

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const registerRouter = require('../auth/registerRouter.js');
 const loginRouter = require('../auth/loginRouter.js');
+const logoutRouter = require('../auth/logoutRouter.js');
 const usersRouter = require('../users/users-router.js');
 
 // /api/register
@@ -9,6 +10,9 @@ router.use('/register', registerRouter);
 
 // /api/login
 router.use('/login', loginRouter);
+
+// api/logout
+router.use('/logout', logoutRouter);
 
 // /api/users
 router.use('/users', usersRouter);
